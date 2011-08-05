9a30c0d74a01bf8acfbc3132cbcf56f65d54b889
Ricardo J. Vazquez <rix501@gmail.com>
2011-08-05 21:38:27 +0000

    Added some libs (node-rss). Can show image of latest xkcd

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 //npm libraries
 var express = require('express');
+var rss = require("./node-rss");
 
 var app = express.createServer();
 
@@ -24,7 +25,31 @@ app.configure('production', function(){
   
 });
 
+
+var feed_url = 'http://xkcd.com/rss.xml';
+
+//var response = rss.parseURL(feed_url, function(articles) {
+//    console.log(articles.length);
+    //for(i=0; i<articles.length; i++) {
+    //console.log("Article: "+i+", "+
+		 //articles[i].title+"\n"+
+		 //articles[i].link+"\n"+
+		 //articles[i].description+"\n"+
+		 //articles[i].content
+		//);
+ //   }
+//});
+
+
+
 // Routes
+
+app.get('/photo',function(req,res){
+    rss.parseURL(feed_url, function(articles) {
+        res.send(articles[0].description);
+    });
+});
+
 
 
 app.listen(process.env.C9_PORT || process.env.PORT || 8001);
