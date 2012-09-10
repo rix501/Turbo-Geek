c142e4573a037d10737f8225b96201e02724f2a9
Ricardo J. Vazquez <rix501@gmail.com>
2012-09-09 23:45:05 -0400

    rollbacks ftw

--- sql/Subscribe.sql
+++ sql/Subscribe.sql
@@ -6,6 +6,8 @@ DROP PROCEDURE IF EXISTS Subscribe;
 
 CREATE PROCEDURE Subscribe(IN p_userID INT, IN p_comicID INT)
 BEGIN
+    DECLARE EXIT handler for sqlexception ROLLBACK;
+
     START TRANSACTION;
         INSERT INTO subscriptions
         (
--- sql/Unsubscribe.sql
+++ sql/Unsubscribe.sql
@@ -6,6 +6,8 @@ DROP PROCEDURE IF EXISTS Unsubscribe;
 
 CREATE PROCEDURE Unsubscribe(IN p_userID INT, IN p_comicID INT)
 BEGIN
+    DECLARE EXIT handler for sqlexception ROLLBACK;
+
     START TRANSACTION;
         DELETE FROM subscriptions
         WHERE userID = p_userID
